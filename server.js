fbfa5ee414cd3b7431a515855d9eb489338f0c1e
ludo046 <ludomoissinac@icloud.com>
2021-09-15 15:50:13 +0200

    host

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const helmet = require('helmet')
 const fs = require('fs')
 require('dotenv').config();
-const db = require("./models");
+//const db = require("./models");
 //db.sequelize.sync();
 //db.sequelize.sync({force: true});
 require('dotenv').config();
