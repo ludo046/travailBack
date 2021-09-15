3c49e2a6ea881d154480addf3a3d88081d5eb21d
ludo046 <ludomoissinac@icloud.com>
2021-09-15 13:56:56 +0200

    host

--- models/index.js
+++ models/index.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(
   {
     host: process.env.HOST,
     dialect: dbConfig.dialect,
-    //operatorsAliases: false,
+    operatorsAliases: 0,
 
     pool: {
       max: dbConfig.pool.max,
