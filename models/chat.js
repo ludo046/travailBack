5a034974529f00e3fe65bb8b3f5605cb9c1043cb
ludo046 <ludomoissinac@icloud.com>
2021-09-21 15:50:06 +0200

    host

--- models/chat.js
+++ models/chat.js
@@ -66,7 +66,7 @@ module.exports = (sequelize, Sequelize) => {
           fields: [{ name: "createdAt" }],
         },
         {
-          name: "fk_ressource_user_idx",
+          name: "fk_chat_user_idx",
           using: "BTREE",
           fields: [{ name: "userId" }],
         },
