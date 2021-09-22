7d94a8759c4de4a6e36c9313c051bd87bd82f59c
ludo046 <ludomoissinac@icloud.com>
2021-09-22 14:07:12 +0200

    assos

--- migrations/20210602140621-create-chats.js
+++ migrations/20210602140621-create-chats.js
@@ -10,7 +10,7 @@ module.exports = {
       },
       userId: {
         type: Sequelize.INTEGER,
-        allowNull: true,
+        allowNull: false,
         references: {
           model: 'Users',
           key: 'id',
