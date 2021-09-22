22308f01a073deb4985a4310cfb4f43c8c19b610
ludo046 <ludomoissinac@icloud.com>
2021-09-22 14:28:35 +0200

    assos

--- routes/chat.js
+++ routes/chat.js
@@ -96,7 +96,7 @@ module.exports = {
           include: [
             {
               model: models.User,
-              //as: "user_chat",
+              as: "user_chat",
               attributes: ["firstname", "lastname", "picture"],
             }
           ],
