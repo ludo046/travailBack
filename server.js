d3814b06191f942852b3ee3cb9d2a509023a757a
ludo046 <ludomoissinac@icloud.com>
2021-09-23 15:39:25 +0200

    mailgun

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ server.use(helmet());
 
 //configure routes
 server.use('/', function(req,res,next){
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://travailaveclesourire.fr');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     res.setHeader('Access-Control-Allow-Credentials: true');
