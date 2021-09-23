0987cd9d438540cfb7733fef261b0d0accc20d5e
ludo046 <ludomoissinac@icloud.com>
2021-09-23 17:41:39 +0200

    mailgun

--- routes/userCtrl.js
+++ routes/userCtrl.js
@@ -54,6 +54,10 @@ module.exports = {
                                 let token = jwtUtils.generateTokenForUser(newUser);
                                let transport = nodemailer.createTransport({
                                    service:"Gmail",
+                                   host: 'smtp.Gmail.com',
+                                   port: 587,
+                                   secure: true,
+                                   requireTLS: true,
                                    auth: {
                                        user: process.env.USER,
                                        pass: process.env.PASS
