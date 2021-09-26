ecddfeff1724dc55efe057cff4b8f3d95d4fd494
ludo046 <ludomoissinac@icloud.com>
2021-09-26 18:25:47 +0200

    mailgun

--- routes/userCtrl.js
+++ routes/userCtrl.js
@@ -111,7 +111,7 @@ module.exports = {
 
     verificationUser: function(req,res){
         const code = req.body.code;
-        console.log(code);
+        console.log(req.body);
 
         models.User.findOne({
             where: {code: code}
