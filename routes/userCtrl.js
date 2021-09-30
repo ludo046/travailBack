32569ad6b1051b0e4b08a35d352c6c8c30fdb6de
ludo046 <ludomoissinac@icloud.com>
2021-09-30 11:00:05 +0200

    message erreurs

--- routes/userCtrl.js
+++ routes/userCtrl.js
@@ -10,7 +10,6 @@ const nodemailer = require('nodemailer'),
       fs = require('fs'),
       hogan = require('hogan.js'),
       inlineCss = require('inline-css');
-const template = require('./template');
 require("dotenv").config();
 
 
@@ -73,7 +72,7 @@ module.exports = {
                                (async function(){
                                    try{
 
-                                    const templateFile = fs.readFileSync(template.template);
+                                    const templateFile = fs.readFileSync('./template/template');
                                     const templateStyled = await inlineCss(templateFile.toString(), {url: "file://"+__dirname+"/template/"});
                                     const templateCompiled = hogan.compile(templateStyled);
                                     const templateRendered = templateCompiled.render({name: newUser.firstname, code: code});
